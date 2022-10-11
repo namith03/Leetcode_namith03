7e3047f461ea9633f6e48d180a58803583f891fa
namith03 <90406910+namith03@users.noreply.github.com>
2022-10-11 12:43:18 -0500

    Time: 120 ms (15.49%), Space: 24.6 MB (15.60%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-       unordered_map<string, vector<string>> map;
-       for(int i = 0; i < strs.size(); i++){
-           string key = helper(strs[i]);
-           map[key].push_back(strs[i]);
-       }
+        unordered_map<string, vector<string>> map;
         vector<vector<string>> result;
-        for(auto iter = map.begin() ; iter !=  map.end(); iter++){
-            result.push_back(iter -> second);
+        for(int i = 0; i < strs.size(); i++){
+            string key = helper(strs[i]);
+            map[key].push_back(strs[i]);
+        }
+        for(auto itr = map.begin(); itr != map.end(); itr++){
+            result.push_back(itr -> second);
         }
         return result;
     }
     string helper(string str){
-        int count[26] = { 0 }; 
-        string key1 = " ";
-        for(int j = 0; j < str.length(); j++)
-           count[str[j] - 'a']++;
-        for(int k = 0; k < 26; k++)
-        {
-            key1.append(to_string(count[k] + 'a'));
+            int count[26] = { 0 };
+            string key1;
+            for(int j = 0; j < str.length(); j++){
+                count[str[j] - 'a']++;
+}
+            for(int k = 0; k < 26; k++){
+                key1.append(to_string(count[k] + 'a'));
+}
+            return key1;
         }
-
-        return key1;
-    }
 };
