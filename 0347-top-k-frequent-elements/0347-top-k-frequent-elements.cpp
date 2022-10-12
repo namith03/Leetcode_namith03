7ffd03fca746a44e60cf24cbc1aea8b317418ebb
namith03 <90406910+namith03@users.noreply.github.com>
2022-10-12 12:35:35 -0500

    Time: 35 ms (35.13%), Space: 15.5 MB (7.92%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -2,22 +2,20 @@ class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
         int n = nums.size();
-        vector<int> ans;
-        unordered_map<int,int> map;
-        for(int i = 0; i < nums.size(); i++){
+        unordered_map<int, int> map;
+        for(int i = 0; i < n; i++)
             map[nums[i]]++;
-}
         vector<vector<int>> freq(n+1);
-        for( auto iter = map.begin(); iter != map.end(); iter++){
-            freq[iter -> second].push_back(iter -> first);
-        }
-        for(int i = nums.size(); i > 0; i--){
-            if(ans.size() >= k)
+        for( auto iter = map.begin(); iter != map.end(); iter++)
+            freq[iter->second].push_back(iter->first);
+        vector<int> result;
+        for(int i = n; i > 0; i--){
+            if(result.size() >= k)
                 break;
             if(!freq[i].empty()){
-                ans.insert(ans.end(), freq[i].begin(), freq[i].end());
+                result.insert(result.end(), freq[i].begin(), freq[i].end());
             }
 }
-        return ans;
+        return result;
     }
 };
