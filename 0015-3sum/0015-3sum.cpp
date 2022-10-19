4ffffd007605659934322dc6e8ae768a859be6fe
namith03 <90406910+namith03@users.noreply.github.com>
2022-10-19 12:11:33 -0500

    Time: 90 ms (92.33%), Space: 19.8 MB (85.26%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -3,28 +3,26 @@ public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         vector<vector<int>> result;
         sort(nums.begin(), nums.end());
-        for(int i = 0; i < nums.size() - 2; i++){
+        for(int i = 0; i < nums.size(); i++){
             if(nums[i] > 0)
                 break;
-            if(i > 0 && nums[i] == nums[i-1])
+            if( i > 0 && nums[i] == nums[i-1])
                 continue;
             int left = i+1;
-            int right = nums.size() - 1;
+            int right = nums.size()-1;
             while(left < right){
-                if((nums[left] + nums[right] + nums[i]) < 0){
-                   left++;
-                }
-                else if((nums[left] + nums[right] + nums[i]) > 0){
+                if(nums[left] + nums[right] + nums[i] < 0)
+                    left++;
+                else if(nums[left] + nums[right] + nums[i] > 0)
                     right--;
-                }
                 else{
-                     result.push_back({nums[i], nums[left], nums[right]});
-                     int dum_left = nums[left], dum_right = nums[right]; 
-                     while(left < right && nums[left] == dum_left )
-                         left++;
-                     while(left < right && nums[right] == dum_right)
-                         right--;
-                } 
+                    result.push_back({nums[i], nums[left], nums[right]});
+                    int dup_left = nums[left], dup_right = nums[right];
+                    while(left < right && nums[left] == dup_left)
+                        left++;
+                    while(left < right && nums[right] == dup_right)
+                        right--;
+                }
             }
 }
         return result;
