5a5c4d47f56c679b9c6807633ab9c41327c0a74d
namith03 <90406910+namith03@users.noreply.github.com>
2022-11-08 12:15:37 -0600

    Time: 70 ms (19.96%), Space: 8.4 MB (21.22%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,40 +1,38 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        if(t.length() > s.length())
-            return "";
         unordered_map<char,int> map1;
-        unordered_map<char,int>map2;
-        int i = 0, j = 0;
-        int result = INT_MAX;
+        unordered_map<char,int> map2;
+        int left = 0, right = 0;
+        int result = INT_MAX,count = 0;
         string res_str = "";
-        int count = 0;
-        for(int k = 0; k < t.length(); k++){
-            map1[t[k]]++;
-        }
-        while(j < s.length()){
-            if(map1.find(s[j]) != map1.end()){
-                map2[s[j]]++;
-                if(map2[s[j]] <= map1[s[j]])
+        for(int i = 0; i < t.length(); i++)
+            map1[t[i]]++;
+        while( right < s.length()){
+            if(map1.find(s[right]) != map1.end()){
+                map2[s[right]]++;
+                if(map2[s[right]] <= map1[s[right]])
                     count++;
             }
             if(count == t.length()){
-                while(map1.find(s[i]) == map1.end() || map2[s[i]] > map1[s[i]]){
-                    map2[s[i]]--;
-                    i++;
+                while(map1.find(s[left]) == map1.end() || map2[s[left]] > map1[s[left]]){
+                    if(map1.find(s[left]) != map1.end()){
+                        map2[s[left]]--;
+                    }
+                    left++;
                 }
-                if(j-i+1 < result){
-                    result = min(j-i+1, result);
-                    res_str = s.substr(i, result);
+                if(right - left + 1 < result){
+                    result = min(right-left+1, result);
+                    res_str = s.substr(left, result);
                 }
-                map2[s[i]]--;
-                if(map1.find(s[i]) != map1.end())
+                if(map1.find(s[left]) != map1.end()){
+                    map2[s[left]]--;
                     count--;
-                i++; 
-}
-            j++;
+                }
+                left++;
+            }
+            right++;
         }
         return res_str;
     }
-    };
-        
+};
