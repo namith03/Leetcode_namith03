bcd7a68e6faaaa68d0e89f57cccdbed894026d83
namith03 <90406910+namith03@users.noreply.github.com>
2022-11-22 16:42:29 -0600

    Time: 671 ms (10.26%), Space: 13.2 MB (74.45%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -10,20 +10,68 @@
  */
 class Solution {
 public:
-    ListNode* merge(ListNode* l1, ListNode* l2){
-    if(l1 == NULL)
-        return l2;
-    if(l2 == NULL)
-        return l1;
-    if(l1 -> val <= l2 -> val){
-        l1 -> next = merge(l1 -> next, l2);
-        return l1;
+    ListNode* merge(ListNode* list1, ListNode* list2){
+        ListNode* temp1 = list1;
+        int size1 = 0, size2 = 0;
+        ListNode* temp2 = list2;
+        while(temp1 != NULL){
+            size1++;
+            temp1 = temp1 -> next;
+        }
+        while(temp2 != NULL){
+            size2++;
+            temp2 = temp2 -> next;
+        }
+        if(size1 == 0 && size2 == 0)
+            return NULL;
+        if(size1 == 0)
+            return list2;
+        if(size2 == 0)
+            return list1;
+        int size = size1 + size2;
+        ListNode* head = new ListNode();
+        if(list1 -> val <= list2 -> val){
+            head -> val = list1 -> val;
+            head -> next = NULL;
+            list1 = list1 -> next;
+            size1--;
 }
-    else{
-        l2 -> next = merge(l1, l2 -> next);
-        return l2;
+        else{
+            head -> val = list2 -> val;
+            head -> next = NULL;
+            list2 = list2 -> next;
+            size2--;
+        }
+        ListNode* temp = head;
+        while(list1 != NULL && list2 != NULL){
+            if(list1-> val <= list2 -> val){
+                temp -> next = list1;
+                temp = temp -> next;
+                list1 = list1 -> next;
+                size1--;
+        }
+            else if (list1 -> val > list2 -> val){   
+                   temp -> next = list2;
+                   temp = temp -> next;
+                   list2 = list2 -> next;
+                   size2--;
+        }
+        }
+          while(size1 > 0){
+                temp -> next = list1;
+                temp = temp -> next;
+                list1 = list1 -> next;
+                size1--;
+            }
+            while(size2 > 0){
+                temp -> next = list2;
+                temp = temp -> next;
+                list2 = list2 -> next;
+                size2--;
+            }
+            temp -> next = NULL;
+        return head;
     }
-}
     ListNode* mergeKLists(vector<ListNode*>& lists) {
        int k = lists.size();
         if(k == 0)
