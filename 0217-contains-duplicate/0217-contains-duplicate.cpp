1ff22361566aa16e75bcc666d59fd5c170b6973d
namith03 <90406910+namith03@users.noreply.github.com>
2023-02-08 10:44:50 -0600

    Time: 147 ms (42.09%), Space: 69.3 MB (26.62%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_map<int, int> freq;
-        for(int i = 0; i < nums.size(); i++)
-            freq[nums[i]]++;
+        unordered_map<int,int> map;
         for(int i = 0; i < nums.size(); i++){
-             if(freq[nums[i]] > 1){
-                 return true;
-             }
+            map[nums[i]]++;
+            if(map[nums[i]] > 1)
+                return true;
         }
         return false;
-           
     }
 };
