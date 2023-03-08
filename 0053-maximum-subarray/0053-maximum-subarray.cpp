19cd50e0e07f4b661018c6dfab716e949ea0ac76
namith03 <90406910+namith03@users.noreply.github.com>
2023-03-08 13:28:29 -0600

    Time: 119 ms (62.64%), Space: 67.8 MB (63.72%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -4,13 +4,10 @@ public:
         int maximum = nums[0];
         int running_max = nums[0];
         for(int i = 1 ; i < nums.size(); i++){
-            int dummy = max(nums[i],running_max + nums[i]);
-            if(dummy >= maximum){
-                maximum = dummy;
-                running_max = dummy;
-            }
-            else
-                running_max = max(nums[i],running_max + nums[i]);
+            if(max(nums[i],running_max + nums[i]) >= maximum)
+                maximum = max(nums[i],running_max + nums[i]);
+                              
+            running_max = max(nums[i],running_max + nums[i]);
         }
         return maximum;
     }
