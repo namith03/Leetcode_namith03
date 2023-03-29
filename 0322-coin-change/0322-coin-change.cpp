4b1bc1b1957dc93dfed98a3514962c15706abda6
namith03 <90406910+namith03@users.noreply.github.com>
2023-03-28 21:08:04 -0500

    Time: 52 ms (95.47%), Space: 14.5 MB (75.93%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        //base case - if amount is equal to value in coin
         vector<int> DP(amount + 1, amount+1);
         DP[0] = 0;
         for (int i = 0; i < coins.size(); ++i) {
