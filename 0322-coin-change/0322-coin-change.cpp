08a8f12647247b277a02a59ae18e687ef2ade762
namith03 <90406910+namith03@users.noreply.github.com>
2023-03-28 19:54:54 -0500

    Time: 61 ms (93.46%), Space: 14.6 MB (63.45%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -2,31 +2,18 @@ class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
         //base case - if amount is equal to value in coin
-        if(amount == 0)
-            return 0;
-        vector<int> dp(amount+1,0);
-        sort(coins.begin(), coins.end());
-        for(int i = 0; i < coins.size(); i++){
-            if(coins[i] <= amount)
-                dp[coins[i]] = 1;
-        }
-        
-        for(int i = 1; i <= amount; i++){
-            if(dp[i] == 0){
-                int minimum = amount + 1;
-                for(int j = 0; j < coins.size(); j++ ){
-                 if(i - coins[j] <= 0)
-                     break;
-                else
-                    minimum = min(minimum,dp[i-coins[j]]);
-            }
-                dp[i] = 1 + minimum;
+        vector<int> DP(amount + 1, amount+1);
+        DP[0] = 0;
+        for (int i = 0; i < coins.size(); ++i) {
+            int coin_val = coins[i];
+            for (int j = coin_val; j <= amount; ++j) {
+                //if (j - coin_val < 0) continue;
+                DP[j] = min(DP[j], DP[j - coin_val] + 1);
             }
         }
-        if(dp[amount] > amount)
-            return -1;
+        if (DP[amount] <= amount)
+            return DP[amount];
         else
-            return dp[amount];
-        
+            return -1;
     }
 };
