333c79c6cd9e8e5c263b764e8ada4c7450d5f84d
namith03 <90406910+namith03@users.noreply.github.com>
2023-04-01 21:43:41 -0500

    Time: 7 ms (34.71%), Space: 11.2 MB (45.02%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,30 +1,26 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int l = 0, r = nums.size()-1;
-        while(l < r){
-            int mid = (l+r)/2;
-            if(nums[mid] > nums[r])
-                l = mid + 1;
-            else
-                r = mid;
-        }
-        //index of min element
-        int min = l;
-        l = 0, r = nums.size()-1;
-        if(target <= nums[r])
-            l = min;
-        else
-            r = min;
-        while(l <= r){
-            int mid = (l+r)/2;
-            if( target == nums[mid])
+        int size = nums.size();
+        int l = 0;
+        int r = size - 1;
+        while( l <= r){
+            int mid = l + ((r-l)/2);
+            if(target == nums[mid])
                 return mid;
-            else if(target > nums[mid])
-                l = mid + 1;
-            else
-                r = mid - 1;
+            else if(nums[mid] >= nums[l]){
+                if(target >= nums[l] && target < nums[mid])
+                    r = mid - 1;
+                else
+                    l = mid + 1;
+            }
+            else{
+                if(target > nums[mid] && target <= nums[r])
+                    l = mid + 1;
+                else
+                    r = mid - 1;
+            }
         }
-         return -1;   
+        return -1;
     }
 };
