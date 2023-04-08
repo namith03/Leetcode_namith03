eb7e0382a57d75d17ec74760cc6a9948fb6f0042
namith03 <90406910+namith03@users.noreply.github.com>
2023-04-08 12:46:02 -0500

    Time: 6 ms (12.01%), Space: 7.3 MB (5.10%) - LeetHub

--- 0166-fraction-to-recurring-decimal/0166-fraction-to-recurring-decimal.cpp
+++ 0166-fraction-to-recurring-decimal/0166-fraction-to-recurring-decimal.cpp
@@ -2,17 +2,15 @@ class Solution {
 public:
     string fractionToDecimal(int n, int d) {
         string result = "";
-        if((n < 0 && d > 0) || (n>0 && d < 0)){
-            result = '-' + result;
-        }
+        if((n < 0 && d > 0) || (n>0 && d <0))
+            result = "-" + result;
         n = abs(n);
         d = abs(d);
         long int quo = n / d;
         long int rem = n % d;
         result = result + to_string(quo);
-        if(rem == 0)
-            return result;
-        result = result + ".";
+        if(rem != 0)
+            result = result + ".";
         unordered_map<int,int>map;
         while(rem != 0 && map.find(rem) == map.end()){
             map[rem] = result.length();
@@ -20,10 +18,10 @@ public:
             quo = rem / d;
             result = result + to_string(quo);
             rem = rem % d;
-        }
-        if(rem != 0){
-            result = result.substr(0,map[rem]) + '('+result.substr(map[rem],result.length() - map[rem]) + ')';
-        } 
+}
+        if(rem != 0)
+            result = result.substr(0,map[rem]) + '(' + result.substr(map[rem], result.length() - map[rem]) + ')';
+        
         return result;
-        }
+    }
 };
