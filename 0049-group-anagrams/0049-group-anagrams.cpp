6f54ed3c8d1dd29da85e2f7182539b1000123057
namith03 <90406910+namith03@users.noreply.github.com>
2023-04-09 16:27:52 -0500

    Time: 267 ms (5.01%), Space: 84.4 MB (5.00%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -3,9 +3,11 @@ public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         unordered_map<string,vector<string>> map;
         vector<vector<string>> result;
+        vector<int> cnt(26,0);
         for(int i = 0; i < strs.size(); i++){
-            vector<int> cnt(26,0);
             string key = "";
+            for(int j = 0; j < 26; j++)
+                cnt[j] = 0;
             for(int j = 0; j < strs[i].length(); j++){
                 cnt[strs[i][j] - 'a']++;
             }
@@ -14,6 +16,7 @@ public:
             //cout << key << endl;
             map[key].push_back(strs[i]);
         }
+        
         for(auto itr = map.begin(); itr != map.end(); itr++){
             result.push_back(itr->second);
         }
