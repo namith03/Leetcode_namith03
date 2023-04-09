e8db0cf8f8d90883ce8d8d34cef883fce53321e3
namith03 <90406910+namith03@users.noreply.github.com>
2023-04-09 15:11:21 -0500

    Time: 42 ms (47.60%), Space: 19.6 MB (82.04%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
-    string helper(string temp){
-            sort(temp.begin(), temp.end());
-            return temp;
-        }
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         unordered_map<string,vector<string>> map;
         vector<vector<string>> result;
-        string st;
         for(int i = 0; i < strs.size(); i++){
-            st = helper(strs[i]);
-            map[st].push_back(strs[i]);
+            string s1 = strs[i];
+            sort(s1.begin(),s1.end());
+            map[s1].push_back(strs[i]);
         }
-        for(auto it = map.begin(); it != map.end(); it++){
-            result.push_back(it->second);
+        for(auto itr = map.begin(); itr != map.end(); itr++){
+            result.push_back(itr->second);
         }
         return result;
     }
