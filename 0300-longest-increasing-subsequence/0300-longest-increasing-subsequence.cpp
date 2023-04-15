55aa6266d76343663734a6ae3b81373d559ad3ae
namith03 <90406910+namith03@users.noreply.github.com>
2023-04-15 12:03:06 -0500

    Time: 14 ms (80.68%), Space: 10.4 MB (93.48%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        vector<int>dp(n,1);
-        int result = 1;
-        for(int i = 1; i < n; i++){
-            for(int j = 0; j < i; j++){
-                if(nums[j] < nums[i])
-                    dp[i] = max(dp[i], dp[j] + 1);
-                    result = max(result,dp[i]);
+        vector<int> sub;
+        sub.push_back(nums[0]);
+        for(int i = 1; i < nums.size(); i++){
+            if(sub[sub.size() - 1] < nums[i])
+                sub.push_back(nums[i]);
+            else{
+                //this command finds the element in sub just greater than nums[i]
+                //usually we do binary search for this but this command does the same
+                    auto iter = lower_bound(sub.begin(), sub.end(),nums[i]);
+                    *iter = nums[i];         
+                }
             }
-        }
-        return result;
+        return sub.size();
     }
 };
